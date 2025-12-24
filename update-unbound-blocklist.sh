f9e93937d78d799427a1e70d300aaba4458d68e0
Ahmad Abdul Machrus <240923127+devhunter-git@users.noreply.github.com>
2025-12-24 20:53:50 +0700

    Update unbound-blocklist.sh

--- update-unbound-blocklist.sh
+++ update-unbound-blocklist.sh
@@ -3,9 +3,16 @@
 BASE_DIR="/etc/unbound/unbound.conf.d/malware"
 
 # ===============================
+# FUNCTION LOG DENGAN TIMESTAMP
+# ===============================
+log() {
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
+}
+
+# ===============================
 # 1. KATEGORI
 # ===============================
-CATEGORIES=("malicious" "phishing" "ransomware" "crypto" "abuse" "scam" "suspicious" "redirect" "gambling" "fakenews" "porn")
+CATEGORIES=("malicious" "phishing" "ransomware" "crypto" "abuse" "scam" "suspicious" "redirect" "fraud" "spam" "piracy" "oisd" "devhunter" "gambling" "fakenews" "lgbtq" "ads")
 
 # Buat folder kategori & hapus file lama
 for cat in "${CATEGORIES[@]}"; do
@@ -17,6 +24,25 @@ done
 # 2. LIST URL + RENAME FILE
 # ===============================
 declare -A LISTS
+
+# Ads / Trackers
+LISTS["ads/0Zinc-easylist.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/0Zinc/easylist.fork.conf"
+LISTS["ads/adaway-hosts.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/adaway/hosts.fork.conf"
+LISTS["ads/blocklistproject-hosts.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/blocklistproject/hosts.fork.conf"
+LISTS["ads/craiu-mobiletrackers.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/craiu/mobiletrackers.fork.conf"
+LISTS["ads/crazy-max-spy.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/crazy-max/spy.fork.conf"
+LISTS["ads/DandelionSprout-GameConsole.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/DandelionSprout.GameConsoleAdblockList.conf"
+LISTS["ads/disconnectme-simple-ad.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/disconnectme/simple-ad.fork.conf"
+LISTS["ads/FadeMind-UncheckyAds.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/FadeMind/UncheckyAds.fork.conf"
+LISTS["ads/firebog-AdguardDNS.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/firebog/AdguardDNS.fork.conf"
+LISTS["ads/firebog-Admiral.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/firebog/Admiral.fork.conf"
+LISTS["ads/firebog-Easylist.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/firebog/Easylist.fork.conf"
+LISTS["ads/firebog-PrigentAds.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/firebog/Prigent-Ads.fork.conf"
+LISTS["ads/MajkiIT-SmartTV.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/MajkiIT/SmartTV-ads.fork.conf"
+LISTS["ads/ray-AdguardMobileAds.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/r-a-y/AdguardMobileAds.fork.conf"
+LISTS["ads/sefinek-hosts.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/sefinek.hosts.conf"
+LISTS["ads/ShadowWhisperer-Ads.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/ShadowWhisperer/Ads.fork.conf"
+LISTS["ads/yoyo-ads-trackers.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ads/yoyo/ads-trackers-etc.fork.conf"
 
 # Malicious
 LISTS["malicious/stalkerware-indicators.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/malicious/AssoEchap/stalkerware-indicators.fork.conf"
@@ -44,6 +70,10 @@ LISTS["phishing/rpilist-phishing.conf"]="https://blocklist.sefinek.net/generated
 # Ransomware
 LISTS["ransomware/ransomware.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/ransomware/blocklistproject/ransomware.fork.conf"
 
+# Fakenews
+LISTS["fakenews/StevenBlack-hosts.fork.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/fakenews/StevenBlack/hosts.fork.conf"
+LISTS["fakenews/marktron-hosts.fork.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/fakenews/marktron/hosts.fork.conf"
+
 # Crypto
 LISTS["crypto/crypto-prigent.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/crypto/cryptojacking/firebog/Prigent/Crypto.fork.conf"
 LISTS["crypto/nocoin.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/crypto/cryptojacking/hoshsadiq/adblock-nocoin-list.fork.conf"
@@ -68,27 +98,45 @@ LISTS["suspicious/sefinek-suspicious.conf"]="https://blocklist.sefinek.net/gener
 # Redirect
 LISTS["redirect/redirect.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/redirect/blocklistproject/redirect.fork.conf"
 
+# Fraud
+LISTS["fraud/fraud-blocklist.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/fraud/blocklistproject/hosts.fork.conf"
+
+# Spam
+LISTS["spam/rpilist-spam.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/spam/RPiList/spam-mails.fork.conf"
+LISTS["spam/toxic-domains.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/spam/stopforumspam/toxic-domains-whole.fork.conf"
+LISTS["spam/add-spam.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/spam/FadeMind/add-Spam.fork.conf"
+
+# Piracy
+LISTS["piracy/piracy-project.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/piracy/blocklistproject/piracy.fork.conf"
+LISTS["piracy/sefinek-piracy.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/piracy/sefinek.hosts.conf"
+
 # Gambling
-LISTS["gambling/gambling-stevenblack.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/gambling/StevenBlack/hosts.fork.conf"
-LISTS["gambling/gambling-project.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/gambling/blocklistproject/hosts.fork.conf"
-LISTS["gambling/gambling-majki.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/gambling/MajkiIT/gambling-hosts.fork.conf"
-LISTS["gambling/gambling-sefinek.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/gambling/sefinek.hosts.conf"
-LISTS["gambling/gambling-indonesia.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/gambling/TrustPositif/gambling-indonesia.fork.conf"
-LISTS["gambling/gambling-sefinek2.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/gambling/sefinek.hosts2.conf"
+LISTS["gambling/stevenblack.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/gambling/StevenBlack/hosts.fork.conf"
+LISTS["gambling/blocklistproject.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/gambling/blocklistproject/hosts.fork.conf"
+LISTS["gambling/majkiit.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/gambling/MajkiIT/gambling-hosts.fork.conf"
+LISTS["gambling/sefinek.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/gambling/sefinek.hosts.conf"
+LISTS["gambling/trustpositif-id.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/gambling/TrustPositif/gambling-indonesia.fork.conf"
+LISTS["gambling/sefinek2.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/gambling/sefinek.hosts2.conf"
 
-# Fakenews
-LISTS["fakenews/fakenews-stevenblack.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/fakenews/StevenBlack/hosts.fork.conf"
-LISTS["fakenews/fakenews-marktron.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/fakenews/marktron/hosts.fork.conf"
+# LGBTQ+
+LISTS["lgbtq/lgbtqplus.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/sites/lgbtqplus.conf"
+LISTS["lgbtq/lgbtqplus2.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/sites/lgbtqplus2.conf"
 
-# Porn
-LISTS["porn/porn-stevenblack.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/porn/StevenBlack/porn.fork.conf"
-LISTS["porn/porn-project.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/porn/blocklistproject/porn.fork.conf"
-LISTS["porn/porn-chadmayfield.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/porn/chadmayfield/pi-blocklist-porn-all.fork.conf"
-LISTS["porn/porn-oisd.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/porn/oisd/nsfw.fork.conf"
-LISTS["porn/porn-sefinek.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/porn/sefinek.hosts.conf"
-LISTS["porn/porn-sefinek2.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/porn/sefinek.hosts2.conf"
-LISTS["porn/porn-sinfonietta.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/porn/Sinfonietta/pornography-hosts.fork.conf"
-LISTS["porn/porn-4skin.conf"]="https://blocklist.sefinek.net/generated/v1/unbound/porn/4skinSkywalker/hosts.fork.conf"
+# OISD lists
+LISTS["oisd/big-oisd.conf"]="https://big.oisd.nl/unbound"
+LISTS["oisd/nsfw-oisd.conf"]="https://nsfw.oisd.nl/unbound"
+
+# DevHunter lists
+LISTS["devhunter/hagezi-tif.conf"]="https://raw.githubusercontent.com/devhunter-git/Unbound/refs/heads/main/hagezi-tif.conf"
+LISTS["devhunter/1hosts-lite.conf"]="https://raw.githubusercontent.com/devhunter-git/Unbound/refs/heads/main/1hosts-lite.conf"
+LISTS["devhunter/hagezi-pro.conf"]="https://raw.githubusercontent.com/devhunter-git/Unbound/refs/heads/main/hagezi-pro.conf"
+LISTS["devhunter/hagezi-light.conf"]="https://raw.githubusercontent.com/devhunter-git/Unbound/refs/heads/main/hagezi-light.conf"
+LISTS["devhunter/hagezi-xiaomi-native.conf"]="https://raw.githubusercontent.com/devhunter-git/Unbound/refs/heads/main/hagezi-native-xiaomi.conf"
+LISTS["devhunter/hagezi-apple-native.conf"]="https://raw.githubusercontent.com/devhunter-git/Unbound/refs/heads/main/hagezi-native-apple.conf"
+LISTS["devhunter/hagezi-huawei-native.conf"]="https://raw.githubusercontent.com/devhunter-git/Unbound/refs/heads/main/hagezi-native-huawei.conf"
+LISTS["devhunter/hagezi-oppo-realme-native.conf"]="https://raw.githubusercontent.com/devhunter-git/Unbound/refs/heads/main/hagezi-native-oppo-realme.conf"
+LISTS["devhunter/hagezi-samsung-native.conf"]="https://raw.githubusercontent.com/devhunter-git/Unbound/refs/heads/main/hagezi-native-samsung.conf"
+LISTS["devhunter/hagezi-vivo-native.conf"]="https://raw.githubusercontent.com/devhunter-git/Unbound/refs/heads/main/hagezi-native-vivo.conf"
 
 # ===============================
 # 3. DOWNLOAD, RENAME, CHECK
@@ -100,11 +148,11 @@ for path in "${!LISTS[@]}"; do
     filename=$(basename "$path")
     temp_file="/tmp/$filename"
 
-    echo "[+] Download $category/$filename"
+    log "[+] Download $category/$filename"
     wget --quiet -O "$temp_file" "$url"
 
     if [ ! -s "$temp_file" ]; then
-        echo "[!] Download gagal atau file kosong: $filename"
+        log "[!] Download gagal atau file kosong: $filename"
         ERROR=1
         continue
     fi
@@ -115,18 +163,24 @@ for path in "${!LISTS[@]}"; do
     # Cek sintaks
     unbound-checkconf "$BASE_DIR/$path" > /dev/null 2>&1
     if [ $? -ne 0 ]; then
-        echo "[!] Error syntax: $BASE_DIR/$path"
+        log "[!] Error syntax: $BASE_DIR/$path"
         ERROR=1
     fi
 done
 
 # ===============================
-# 4. RESTART UNBOUND
+# 4. RELOAD / RESTART UNBOUND
 # ===============================
-if [ $ERROR -eq 0 ]; then
-    echo "[✓] Semua file valid, restart Unbound..."
-    systemctl restart unbound
-    echo "[✓] Unbound berhasil direstart!"
+log "[?] Mencoba reload Unbound..."
+systemctl reload unbound
+if [ $? -eq 0 ]; then
+    log "[?] Unbound berhasil direload!"
 else
-    echo "[!] Ada file error, Unbound tidak direstart!"
+    log "[!] Reload gagal, mencoba restart..."
+    systemctl restart unbound
+    if [ $? -eq 0 ]; then
+        log "[?] Unbound berhasil direstart!"
+    else
+        log "[!] Restart juga gagal! Periksa konfigurasi Unbound."
+    fi
 fi
